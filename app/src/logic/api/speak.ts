4ad88c0e85d351a11363d340a475b4793e638c20
shotor <shotor@shotor.com>
2020-05-05 23:17:19 +0200

    feat(app): use actual grpc endpoint

--- app/src/logic/api/speak.ts
+++ app/src/logic/api/speak.ts
@@ -1,12 +1,41 @@
-// temporarily load a sample audio file
-// until grpc set up
-import { Asset } from 'expo-asset'
-const { uri } = Asset.fromModule(require('../../../assets/sample.wav'))
+import * as Crypto from 'expo-crypto'
+import * as FileSystem from 'expo-file-system'
+import { SpeakRequest, SpeakReply } from '../../generated/tihu_pb'
+import { client } from './client'
+import { fs } from '@config/fs'
 
-export const apiSpeak = (value: string) =>
-  new Promise<string>(async (resolve) => {
-    // for now simulate fetch
-    setTimeout(() => {
+export const speak = (
+  text: string,
+  voice: SpeakRequest.Voice = SpeakRequest.Voice.MBROLA_FEMALE
+) =>
+  new Promise<string>(async (resolve, reject) => {
+    const request = new SpeakRequest()
+    request.setVoice(voice)
+    request.setText(text)
+
+    const fileName = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      text
+    )
+
+    const uri = `${fs.downloadPath}/${fileName}`
+
+    const call = client.speak(request)
+
+    let result = ''
+
+    call.on('data', (chunk: SpeakReply) => {
+      result += chunk.getWave_asB64()
+    })
+
+    call.on('error', (err) => {
+      reject(err)
+    })
+
+    call.on('end', async () => {
+      await FileSystem.writeAsStringAsync(uri, result, {
+        encoding: 'base64',
+      })
       resolve(uri)
-    }, 500)
+    })
   })
--- app/src/logic/bootstrap.ts
+++ app/src/logic/bootstrap.ts
@@ -1,8 +1,16 @@
 import { I18nManager } from 'react-native'
+import * as FileSystem from 'expo-file-system'
+import { fs } from '@config/fs'
+import { makeDirectoryAsync } from 'expo-file-system'
 
 /**
  * App bootstrap logic
  */
-export const bootstrap = () => {
+export const bootstrap = async () => {
   I18nManager.forceRTL(true)
+
+  // create download dir
+  if (!(await FileSystem.getInfoAsync(fs.downloadPath)).exists) {
+    await FileSystem.makeDirectoryAsync(fs.downloadPath)
+  }
 }
